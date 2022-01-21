eb3420bfac39b16ac792be50d9fedfdd2d20d185
RayLiu1999 <rray12tw346@gmail.com>
2022-01-21 23:03:48 +0800

    make it better

--- layout/footer.php
+++ layout/footer.php
@@ -1,7 +1,7 @@
 </div>
 <footer class="bg-light text-center text-lg-start">
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-        © 2022 Copyright: Ray Liu
+        Copyright © Ray Liu 2022
     </div>
 </footer>
 </body>
